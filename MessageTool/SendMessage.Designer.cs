5308288b9e252607de43c19e9e0e12e219c44b98
grumpydeveloper007 <grumpydeveloper007>
2018-02-18 11:12:22 +1000

    Initial Checkin

---
+++ MessageTool/SendMessage.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace MessageTool
+{
+    partial class SendMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAccountSid = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPropertySid = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblCharacters = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.butCheckStatus = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.butSend = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtMessageSid = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResponse.Location = new System.Drawing.Point(15, 425);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(488, 100);
+            this.txtResponse.TabIndex = 10;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(85, 137);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(114, 20);
+            this.txtFrom.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Account Sid";
+            // 
+            // txtAccountSid
+            // 
+            this.txtAccountSid.Location = new System.Drawing.Point(85, 19);
+            this.txtAccountSid.Name = "txtAccountSid";
+            this.txtAccountSid.Size = new System.Drawing.Size(260, 20);
+            this.txtAccountSid.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Token";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(85, 45);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(260, 20);
+            this.txtToken.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Property Sid";
+            // 
+            // txtPropertySid
+            // 
+            this.txtPropertySid.Location = new System.Drawing.Point(85, 71);
+            this.txtPropertySid.Name = "txtPropertySid";
+            this.txtPropertySid.Size = new System.Drawing.Size(260, 20);
+            this.txtPropertySid.TabIndex = 3;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(85, 163);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(114, 20);
+            this.txtTo.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(85, 192);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(394, 70);
+            this.txtMessage.TabIndex = 6;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // lblCharacters
+            // 
+            this.lblCharacters.AutoSize = true;
+            this.lblCharacters.Location = new System.Drawing.Point(412, 265);
+            this.lblCharacters.Name = "lblCharacters";
+            this.lblCharacters.Size = new System.Drawing.Size(69, 13);
+            this.lblCharacters.TabIndex = 14;
+            this.lblCharacters.Text = "<characters>";
+            this.lblCharacters.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 409);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Response";
+            // 
+            // butCheckStatus
+            // 
+            this.butCheckStatus.Location = new System.Drawing.Point(382, 16);
+            this.butCheckStatus.Name = "butCheckStatus";
+            this.butCheckStatus.Size = new System.Drawing.Size(94, 23);
+            this.butCheckStatus.TabIndex = 9;
+            this.butCheckStatus.Text = "Check Status";
+            this.butCheckStatus.UseVisualStyleBackColor = true;
+            this.butCheckStatus.Click += new System.EventHandler(this.butCheckStatus_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.butSend);
+            this.groupBox1.Controls.Add(this.lblCharacters);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtMessage);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtPropertySid);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtToken);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtAccountSid);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtFrom);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 326);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send Message";
+            // 
+            // butSend
+            // 
+            this.butSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butSend.Location = new System.Drawing.Point(404, 297);
+            this.butSend.Name = "butSend";
+            this.butSend.Size = new System.Drawing.Size(75, 23);
+            this.butSend.TabIndex = 7;
+            this.butSend.Text = "Send Message";
+            this.butSend.UseVisualStyleBackColor = true;
+            this.butSend.Click += new System.EventHandler(this.butSend_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.txtMessageSid);
+            this.groupBox2.Controls.Add(this.butCheckStatus);
+            this.groupBox2.Location = new System.Drawing.Point(15, 344);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(484, 52);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Last Message";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Message Sid";
+            // 
+            // txtMessageSid
+            // 
+            this.txtMessageSid.Location = new System.Drawing.Point(82, 19);
+            this.txtMessageSid.Name = "txtMessageSid";
+            this.txtMessageSid.Size = new System.Drawing.Size(260, 20);
+            this.txtMessageSid.TabIndex = 8;
+            // 
+            // SendMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 537);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtResponse);
+            this.Name = "SendMessage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Message Sender";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SendMessage_FormClosed);
+            this.Load += new System.EventHandler(this.SendMessage_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAccountSid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPropertySid;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblCharacters;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button butCheckStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button butSend;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMessageSid;
+    }
+}
+
---
+++ MessageTool/SendMessage.cs
@@ -0,0 +1,82 @@
+﻿using FoneDynamics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageTool
+{
+    public partial class SendMessage : Form
+    {
+
+
+        public SendMessage()
+        {
+            InitializeComponent();
+        }
+
+        private void butSend_Click(object sender, EventArgs e)
+        {
+            string from = txtFrom.Text;
+            if (string.IsNullOrWhiteSpace(from))
+            {
+                from = null;
+            }
+
+            var message = new FoneDynamics.Rest.V2.MessageResource(txtTo.Text.Trim(), txtMessage.Text, from);
+            FoneDynamicsClient.Initialize(txtAccountSid.Text.Trim(), txtToken.Text.Trim(), txtPropertySid.Text.Trim());
+
+
+            try
+            {
+                var response = FoneDynamics.Rest.V2.MessageResource.Send(message);
+                txtResponse.Text = response.Status.ToString();
+                txtMessageSid.Text = response.MessageSid;
+            }
+            catch (Exception ex)
+            {
+                txtResponse.Text = ex.Message;
+            }
+
+        }
+
+        private void SendMessage_Load(object sender, EventArgs e)
+        {
+            txtAccountSid.Text = Properties.Settings.Default.AccountSid;
+            txtToken.Text = Properties.Settings.Default.Token;
+            txtPropertySid.Text = Properties.Settings.Default.PropertySid;
+        }
+
+        private void SendMessage_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.AccountSid = txtAccountSid.Text;
+            Properties.Settings.Default.Token = txtToken.Text;
+            Properties.Settings.Default.PropertySid = txtPropertySid.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            lblCharacters.Text = txtMessage.Text.Length.ToString();
+        }
+
+        private void butCheckStatus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var response = FoneDynamics.Rest.V2.MessageResource.Get(txtMessageSid.Text);
+                txtResponse.Text = response.Status.ToString();
+            }
+            catch (Exception ex)
+            {
+                txtResponse.Text = ex.Message;
+            }
+
+        }
+    }
+}
